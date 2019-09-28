aedfc0effaf53e73c54f8439aa1f6955505b1787
saeedeldah <saeed.eldah@gmail.com>
2019-09-28 13:54:41 +0300

    intra and inter new

--- 4-evaluation/categoy_distance_ditribution.py
+++ 4-evaluation/categoy_distance_ditribution.py
@@ -19,7 +19,7 @@ def convert_to_float(string_vector):
     return vector
 
 
-def get_category_features_dictionary_from_complete_features(input_file_path):
+def get_category_all_features_dictionary(input_file_path):
     category_features = {}
     with open(input_file_path) as f:
         data_set_count = sum(1 for line in f) - 1
@@ -44,7 +44,7 @@ def get_category_features_dictionary_from_complete_features(input_file_path):
 
 
 def generate_category_feature(input_file_path, output_file_path):
-    category_features = get_category_features_dictionary_from_complete_features(input_file_path)
+    category_features = get_category_all_features_dictionary(input_file_path)
 
     category_average_features = {}
 
@@ -94,7 +94,7 @@ def generate_category_feature(input_file_path, output_file_path):
         f.close()
 
 
-def get_category_features_dictionary(input_file_path):
+def get_category_center_features_dictionary(input_file_path):
     category_features = {}
     with open(input_file_path) as f:
         data_set_count = sum(1 for line in f) - 1
@@ -118,16 +118,40 @@ def get_category_features_dictionary(input_file_path):
     return category_features
 
 
+def get_category_self_distance_dictionary(category_all_features, category_center_features):
+    category_distance_dictionary = {}
+    for category_name_average, category_average_feature_vector in category_center_features.iteritems():
+        category_distance_dictionary[category_name_average] = []
+        for category_complete_feature_vector in category_all_features[category_name_average]:
+            distance = utl.calculate_distance(category_average_feature_vector, category_complete_feature_vector)
+            category_distance_dictionary[category_name_average].append(distance)
+    category_average_distance_dictionary = {}
+    for category, distance_list in category_distance_dictionary.iteritems():
+        category_average_distance_dictionary[category] = mean(distance_list)
+
+    return category_average_distance_dictionary
+
+
+def get_two_categories_distance(category_all_feature_vectors, category_center_feature_vector):
+    category_distance_list = []
+    for category_feature_vector in category_all_feature_vectors:
+        distance = utl.calculate_distance(category_feature_vector, category_center_feature_vector)
+        category_distance_list.append(distance)
+    return mean(category_distance_list)
+
+
 def generate_category_feature_distribution(features_file_path, input_file_path, output_file_path):
-    category_features = get_category_features_dictionary(input_file_path)
-    category_self_distance_dictionary = get_category_self_distance_dictionary(features_file_path, input_file_path)
+    category_center_features = get_category_center_features_dictionary(input_file_path)
+    category_all_features = get_category_all_features_dictionary(features_file_path)
+    category_self_distance_dictionary = get_category_self_distance_dictionary(category_all_features,
+                                                                              category_center_features)
     category_distance_dictionary = {}
     categories = []
-    for category, category_feature_vector in category_features.iteritems():
+    for category, category_feature_vector in category_center_features.iteritems():
         categories.append(category)
-        for category2, category_feature_vector2 in category_features.iteritems():
+        for category2, category_feature_vector2 in category_center_features.iteritems():
             if category != category2:
-                dist = utl.calculate_distance(category_feature_vector, category_feature_vector2)
+                dist = get_two_categories_distance(category_all_features[category], category_center_features[category2])
             else:
                 dist = category_self_distance_dictionary[category]
 
@@ -154,23 +178,6 @@ def mean(numbers):
     return float(sum(numbers)) / float(len(numbers))
 
 
-def get_category_self_distance_dictionary(features_file_path, category_average_file_path):
-    category_complete_features = get_category_features_dictionary_from_complete_features(
-        features_file_path)
-    category_average_features = get_category_features_dictionary(category_average_file_path)
-    category_distance_dictionary = {}
-    for category_name_average, category_average_feature_vector in category_average_features.iteritems():
-        category_distance_dictionary[category_name_average] = []
-        for category_complete_feature_vector in category_complete_features[category_name_average]:
-            distance = utl.calculate_distance(category_average_feature_vector, category_complete_feature_vector)
-            category_distance_dictionary[category_name_average].append(distance)
-    category_average_distance_dictionary = {}
-    for category, distance_list in category_distance_dictionary.iteritems():
-        category_average_distance_dictionary[category] = mean(distance_list)
-
-    return category_average_distance_dictionary
-
-
 def main():
     input_output_list = [
         ["./features/normalized_shot_features.csv", "category_average_shot_features.csv"],
@@ -192,19 +199,6 @@ def main():
     for input_output in input_output_list2:
         print "generating " + input_output[1]
         generate_category_feature_distribution(input_output[0], input_output[1], input_output[2])
-
-    # input_output_list3 = [
-    #     ["./features/normalized_shot_features.csv", "category_average_shot_features.csv",
-    #      "category_shot_self_distance.csv"],
-    #     ["./features/normalized_video_features.csv", "category_average_video_features.csv",
-    #      "category_video_self_distance.csv"],
-    #     ["./features/normalized_complete_video_features.csv", "category_average_complete_video_features.csv",
-    #      "category_complete_video_self_distance.csv"]
-    # ]
-    # for input_output in input_output_list3:
-    #     print "generating " + input_output[1]
-    #     generate_category_self_distance_csv(input_output[0], input_output[1], input_output[2])
-    #     break
 
 
 main()
